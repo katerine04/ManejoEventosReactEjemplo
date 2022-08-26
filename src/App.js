0a890370d281f864cd8b7b46afc11cdb48c2ec76
katerine04 <katerine.arias@telusinternational.com>
2022-08-26 08:50:45 -0600

    uploading files

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <form onSubmit={onClick}>
+        <p>Ingrese el primer valor:
+          <input type="number" name="valor1"></input>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Ingrese el segundo valor:
+          <input type="number" name="valor2"></input>
+        </p>
+        <p>
+          <input type="submit" value="Sumar"></input>
+        </p>
+      </form>
     </div>
   );
+}
+
+function onClick(e){
+  e.preventDefault();
+  const num1= parseInt(e.target.valor1.value, 10);
+  const num2= parseInt(e.target.valor2.value, 10);
+  const suma = num1 + num2;
+
+  alert('La suma es de: ' + suma);
 }
 
 export default App;
